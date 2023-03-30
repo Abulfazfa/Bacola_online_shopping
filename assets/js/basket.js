ab4a5f87b06b75a5f137c91291645c4e88157baf
unknown <abulfazfa@code.edu.az>
2023-03-30 19:11:11 +0400

    Deals of the week

---
+++ assets/js/basket.js
@@ -0,0 +1,156 @@
+let table = document.querySelector('.table');
+let emtyMessage = document.querySelector('.emptyPage');
+let totalPriceArea = document.getElementById('totalPrice');
+let total = 0;
+
+
+
+if (localStorage.getItem('basket') != null) {
+    EmptyBasket(1);
+    let arr = JSON.parse(localStorage.getItem('basket'));
+    arr.forEach(product => {
+        //#region a
+        let tr = document.createElement("tr");
+        let tdImg = document.createElement('td');
+        tdImg.innerHTML = `<img src="${product.imgUrl}" alt="" width="150" height="150">`
+        let tdName = document.createElement('td');
+        tdName.innerHTML = `${product.name}`
+        let tdPrice = document.createElement('td');
+        tdPrice.innerHTML = `${product.price}`
+        let tdCount = document.createElement('td');
+        tdCount.innerHTML = `<i class="fa-solid fa-square-minus"></i> <span>${product.count}</span> <i class="fa-solid fa-square-plus"></i>`
+        let tdRemove = document.createElement('td');
+        tdRemove.innerHTML = `<i class="fa-solid fa-trash"></i>`
+        let productPrice = product.price.slice(0,-4);
+        let totalPrice = `Total Price: $${total += (product.count * productPrice)}`;
+        tr.append(tdImg,tdName,tdPrice,tdCount,tdRemove);
+        table.lastElementChild.appendChild(tr);
+        totalPriceArea.innerHTML = totalPrice;
+        totalPriceArea.classList.remove('d-none');
+        let trash = tr.lastElementChild;
+        trash.classList.add("text-danger");
+        tr.classList.add("text-center");
+
+        trash.lastElementChild.addEventListener("click", function (e) {
+            const i = arr.indexOf(product);
+            if (i > -1) {
+                arr.splice(i, 1);
+            }
+            
+            table.lastElementChild.removeChild(tr);
+            totalPrice = `Total Price: $${total = total - (product.count * productPrice)}`;
+            totalPriceArea.innerHTML = totalPrice;
+            localStorage.setItem('basket',JSON.stringify(arr));
+            calcBasketCount();
+            removeBasket();
+            
+  
+        })
+        
+        //#endregion
+
+
+        tdCount.firstElementChild.style.cursor = "pointer";
+        tdCount.lastElementChild.style.cursor = "pointer";
+        tdCount.firstElementChild.addEventListener("click", function (e){
+            product.count--;
+
+            decraseProduct(product);
+            tdCount.firstElementChild.nextElementSibling.innerText = product.count;
+            total -= Number(productPrice)
+            totalPrice = `Total Price: $${round(total, 2)}`;
+            totalPriceArea.innerHTML = totalPrice;
+            if (product.count == 0) {
+                table.lastElementChild.removeChild(tr);
+                removeProduct(product);
+                calcBasketCount();
+                removeBasket();
+                
+                
+            }
+            
+        })
+        tdCount.lastElementChild.addEventListener("click", function (e){
+            product.count++;
+            increaseProduct(product);
+            tdCount.firstElementChild.nextElementSibling.innerText = product.count;
+            total += Number(productPrice)
+            totalPrice = `Total Price: $${round(total, 2)}`;
+            totalPriceArea.innerHTML = totalPrice;
+            
+        })
+
+    });
+}
+calcBasketCount();
+
+function calcBasketCount() {
+    if (localStorage.getItem('basket') != null) {
+        let arr = JSON.parse(localStorage.getItem('basket'));
+        basketCount.innerText = arr.length;
+    }
+}
+
+
+function removeProduct(product){
+    if(localStorage.getItem('basket') != null){
+        let arr = JSON.parse(localStorage.getItem('basket'));
+        let clickcedProduct = arr.find(p => p.name == product.name);
+        const i = arr.indexOf(clickcedProduct);
+        console.log(i);
+        if (i > -1) {
+            arr.splice(i, 1);
+        }
+        console.log(arr);
+        localStorage.setItem('basket',JSON.stringify(arr));
+        
+    }
+}
+function decraseProduct(product){
+    if(localStorage.getItem('basket') != null){
+        let arr = JSON.parse(localStorage.getItem('basket'));
+        console.log(product.id);
+        let clickcedProduct = arr.find(p => p.name == product.name);
+        
+        clickcedProduct.count--;     
+        console.log(arr);
+        localStorage.setItem('basket',JSON.stringify(arr));
+    }
+}
+
+function increaseProduct(product){
+    if(localStorage.getItem('basket') != null){
+        let arr = JSON.parse(localStorage.getItem('basket'));
+        console.log(product.id);
+        let clickcedProduct = arr.find(p => p.name == product.name);        
+        clickcedProduct.count++;     
+        console.log(arr);
+        localStorage.setItem('basket',JSON.stringify(arr));
+    }
+}
+
+function removeBasket(){
+    if (localStorage.getItem('basket') != null) {
+        let arr = JSON.parse(localStorage.getItem('basket'));
+        if (arr.length == 0) {
+            localStorage.removeItem('basket');
+            EmptyBasket("-1");
+          }
+      }
+}
+function EmptyBasket(number){
+    if(number == 1){
+        table.classList.remove('d-none');
+        emtyMessage.classList.add('d-none');
+    }
+    else{
+        table.classList.add('d-none');
+        emtyMessage.classList.remove('d-none');
+        totalPriceArea.classList.add('d-none');
+    }
+    
+}
+function round(value, precision) {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+}
---
+++ assets/js/script.js
@@ -0,0 +1,98 @@
+///////////basket////////////////
+let addBaskets = document.querySelectorAll('.btn-add-to-cart');
+let basketCount = document.querySelector('.basketCount');
+addBaskets.forEach(addBasket => {  
+    
+    addBasket.addEventListener('click', function(event){
+        console.log("111234546454");
+        event.preventDefault();
+        let id = addBasket.parentNode.parentNode.getAttribute("data-id");
+        let img = addBasket.parentNode.parentNode.firstElementChild.firstElementChild.getAttribute('src');
+        let name = addBasket.parentNode.parentNode.firstElementChild.nextElementSibling.firstElementChild.innerText;
+        let price = addBasket.previousElementSibling.firstElementChild.innerText;
+        if(localStorage.getItem('basket') == null){
+           localStorage.setItem('basket',JSON.stringify([]));
+        }
+        let arr = JSON.parse(localStorage.getItem('basket'));
+        let existProduct = arr.find(p => p.id == id);
+        if(existProduct == undefined){
+            arr.push({
+                id: id,
+                imgUrl: img,
+                name: name,
+                price: price,
+                count: 1
+            });
+        }
+        else{
+            existProduct.count++;
+        }
+        
+        localStorage.setItem('basket',JSON.stringify(arr));
+        calcBasketCount();
+    })
+})
+
+///////////counter////////////
+$(document).ready(function(){
+
+    var countDownDate = new Date("Apr 30, 2023 15:37:25").getTime();
+    
+    var x = setInterval(function() {
+    
+      var now = new Date().getTime();
+
+      var distance = countDownDate - now;
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      if (days < 10) {
+        days = "0" + days;
+      }
+      if (hours < 10) {
+        hours = "0" + hours;
+      }
+      if (minutes < 10) {
+        minutes = "0" + minutes;
+      }
+      if (seconds < 10) {
+        seconds = "0" + seconds;
+      }
+      $(".days").text(days);
+      $(".hours").text(hours);
+      $(".minutes").text(minutes);
+      $(".second").text(seconds);
+        
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+      }
+    }, 1000);
+ 
+  
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+calcBasketCount();
+function calcBasketCount() {
+    if (localStorage.getItem('basket') != null) {
+        let arr = JSON.parse(localStorage.getItem('basket'));
+        basketCount.innerText = arr.length;
+    }
+}
